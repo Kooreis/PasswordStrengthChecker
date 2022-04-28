b967b2972ce30bfe37f20e1f45cc2ad9635b4b5f
Kooreis <kooreis.official@gmail.com>
2022-04-27 23:00:00 -0500

    This update introduces a function to calculate the strength of the password. The function checks if the password has mixed case, contains a digit, and contains a special character. Each of these conditions increases the score by 1.

--- Password_Strength_Checker.cs
+++ Password_Strength_Checker.cs
@@ -1,11 +1,16 @@
-using System;
-using System.Linq;
-
-class Program
-{
-    static void Main(string[] args)
+static int GetPasswordStrength(string password)
     {
-        Console.Write("Enter your password: ");
-        string password = Console.ReadLine();
+        if (password.Length < 8)
+            return 0;
+
+        int score = 1;
+
+        if (HasMixedCase(password))
+            score++;
+        if (HasDigit(password))
+            score++;
+        if (HasSpecialChar(password))
+            score++;
+
+        return score;
     }
-}
