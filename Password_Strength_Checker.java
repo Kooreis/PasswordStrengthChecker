6ecdfd497c1f475510046b11275a24257064846e
Kooreis <kooreis.official@gmail.com>
2022-04-27 23:00:00 -0500

    In the second update, a conditional statement is added to the main method. It checks if the entered password is valid by calling the yet-to-be-defined method 'isValidPassword'. Depending on the result, it prints out whether the password is strong or weak.

--- Password_Strength_Checker.java
+++ Password_Strength_Checker.java
@@ -5,5 +5,10 @@ public class PasswordStrengthChecker {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your password:");
         String password = scanner.nextLine();
+        if (isValidPassword(password)) {
+            System.out.println("Password is strong.");
+        } else {
+            System.out.println("Password is weak.");
+        }
     }
 }
